142f290a841e35f4ba746bca95876576c5b4b0ad
Gan Thiam Chi <thiamchi.gan@gmail.com>
2018-02-17 02:07:17 +0800

    Bluetooth device and address detected

--- BluetoothSniffer/src/main/java/com/techapp/bluetoothsniffer/MainController.java
+++ BluetoothSniffer/src/main/java/com/techapp/bluetoothsniffer/MainController.java
@@ -6,12 +6,20 @@ package com.techapp.bluetoothsniffer;
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.bluetooth.LocalDevice;
+
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.RemoteDevice;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -26,19 +34,50 @@ public class MainController implements Initializable {
     
     @FXML
     private Label lbBluetooth;
-    
+
     @FXML
-    private void handleConnectAction(ActionEvent event){
+    private Button btConnect;
+
+    @FXML
+    private Button btDisconnect;
+
+    @FXML
+    private Button btTest;
+
+    @FXML
+    private TextArea tfDebug;
+
+    @FXML
+    void handleConnectAction(ActionEvent event) {
         lbBluetooth.setText("Connect!");
         logger.info("User clicked Connect!");
     }
-    
+
     @FXML
-    private void handleDisconnectAction(ActionEvent event){
+    void handleDisconnectAction(ActionEvent event) {
         lbBluetooth.setText("Disconnect!");
         logger.info("User clicked Disconnect!");
     }
     
+    private String str1;
+    private String str2;
+    @FXML
+    void handleTestAction(ActionEvent event) throws IOException {
+        try{
+        LocalDevice device = LocalDevice.getLocalDevice();
+        RemoteDevice[] remotedevice = device.getDiscoveryAgent().retrieveDevices(DiscoveryAgent.PREKNOWN);
+        for(RemoteDevice d : remotedevice)
+        {
+            str1 = "Device name : " + d.getFriendlyName(false);
+            str2 = "Bluetooth Address : " + d.getBluetoothAddress() + "\n";
+            logger.info(str1);
+            logger.info(str2);
+        }
+        }
+        catch (Exception e){
+            System.out.print(e);
+        }
+    }
     /**
      * Initializes the controller class.
      */
